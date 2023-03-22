bab3aeef29535227c8beed8beb6f937b827fc1b0
Tatiana Tintar <tintartatiana@gmail.com>
2023-03-22 10:09:10 -0400

    modified:   cypress/e2e/Hotels_menu_sanity test.cy.js

--- cypress/e2e/Hotels_menu_sanity test.cy.js
+++ cypress/e2e/Hotels_menu_sanity test.cy.js
@@ -2,6 +2,12 @@
 describe('Navigate to HOTELS menu and check all options', () => {
     
     const hotels = '.hotels';
+    const search = '[type="submit"]';
+    const destinations = '[name="Filter.DestinationId"]';
+    const adults = '[name="Filter.AdultNum"]';
+    const childrens = '[name="Filter.ChildrenNum"]';
+    const checkin = '[name="Filter.CheckIn"]';
+    const checkout = '[name="Filter.CheckOut"]';
  
     
     before(() => {
@@ -15,32 +21,83 @@ describe('Navigate to HOTELS menu and check all options', () => {
 
   
 
-      it('Check drop down list Destinations ', () => {
-        cy.get('#Filter_DestinationId').should("be.visible")
-        cy.get('#Filter_DestinationId').select('Jerusalem')
-        cy.get('#Filter_DestinationId').select('London')
-        cy.get('#Filter_DestinationId').select('New York')
+      it('Check drop down list Destinations to Jerusalem ', () => {
+        cy.get(destinations).should("be.visible")
+        cy.get(destinations).select('Jerusalem')
+        cy.get(search).click()
       })
 
+      it('Check drop down list Destinations to London ', () => {
+        cy.get(destinations).should("be.visible")
+        cy.get(destinations).select('London')
+        cy.get(search).click()
+      })
+
+      it('Check drop down list Destinations to New York ', () => {
+        cy.get(destinations).should("be.visible")
+        cy.get(destinations).select('New York')
+        cy.get(search).click()
+      })
+        
       it('Check Children options ', () => {
-      cy.get('#Filter_ChildrenNum').should("be.visible")
-      cy.get('#Filter_ChildrenNum').type('4')
-      cy.get('#Filter_ChildrenNum').clear()
-      cy.get('#Filter_ChildrenNum').type('14')
+      cy.get(destinations).select('New York')  
+      cy.get(childrens).should("be.visible")
+      cy.get(childrens).clear()
+      cy.get(childrens).type('4')
+      cy.get(search).click()
+      })
+
+      it('Check Children options - negative test ', () => {
+        cy.get(destinations).select('New York')  
+        cy.get(childrens).should("be.visible")
+        cy.get(childrens).clear()
+        cy.get(childrens).type('1114')
+        cy.get(search).click()
+        })
+
+        it('Check Adults options ', () => {
+          cy.get(destinations).select('New York')  
+          cy.get(adults).should("be.visible")
+          cy.get(adults).clear()
+          cy.get(adults).type('4')
+          cy.get(search).click()
+        })
+
+        it('Check Adults options - negative test ', () => {
+          cy.get(destinations).select('New York')  
+          cy.get(adults).should("be.visible")
+          cy.get(adults).clear()
+          cy.get(adults).type('114')
+          cy.get(search).click()
+        })
+
+        it('Check Check In options', () => {
+          cy.get(destinations).select('New York')  
+          cy.get(checkin).should("be.visible")
+          cy.get(checkin).clear()
+          cy.get(checkin).type('2023-12-04')
+        })
+
+        it('Check Check In options - negative test ', () => {
+          cy.get(destinations).select('New York') 
+          cy.get(checkin).should("be.visible")
+          cy.get(checkin).clear()
+          cy.get(checkin).type('2022-31-03')
+        })
+
+        it('Check Check Out options', () => {
+          cy.get(destinations).select('New York')  
+          cy.get(checkout).should("be.visible")
+          cy.get(checkout).clear()
+          cy.get(checkout).type('2023-12-04')
+        })
+
+        it('Check Check Out options - negative test ', () => {
+          cy.get(destinations).select('New York') 
+          cy.get(checkout).should("be.visible")
+          cy.get(checkout).clear()
+          cy.get(checkout).type('2022-31-03')
+        })
       
-      })
-
-      it('Do negative tests for adults, children and dates options ', () => {
-      cy.get('#Filter_AdultNum').should("be.visible")
-      cy.get('#Filter_AdultNum').clear()
-      cy.get('#Filter_AdultNum').type('-5')
-      cy.get('#Filter_AdultNum').clear()
-      cy.get('#Filter_ChildrenNum').clear()
-      cy.get('#Filter_ChildrenNum').type('-1004')
-      cy.get('#dp1678477092262').should("be.visible")
-      cy.get('#dp1678477092262').type('2023-30-03')
-      cy.get('#dp1679324924343').should("be.visible")
-      cy.get('#dp1679324924343').type('2024-31-03')
-      })
-
+  
 })
